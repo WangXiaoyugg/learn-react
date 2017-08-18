1630d3e93799de90f143ff0d0c9cb1f7fb69b655
王晓雨 <wangxiaoyu@wangxiaoyudeMacBook-Pro.local>
2017-08-18 15:35:02 +0800

    with context must use PropTypes.context

--- redux_basic/src/index.js
+++ redux_basic/src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import ControlPanel from './views/ControlPanel';
 import './index.css';
 
+import store from './Store'
+import Provider from './Provider'
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<ControlPanel />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <ControlPanel />
+    </Provider>,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
